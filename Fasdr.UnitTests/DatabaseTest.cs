e341b78f4d988eb476976c53ce5924197f1dfe72
Michael Kelley <michael.a.kelley@gmail.com>
2015-08-23 21:38:41 -0700

    Adds first unit tests for Database class

---
+++ Fasdr.UnitTests/DatabaseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fasdr.Backend;
+using System.IO.Abstractions.TestingHelpers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Fasdr.UnitTests
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void TestConfigLoadFileNotFound()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>{ });
+            var db = new Database(fileSystem);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestConfigLoadFileCorruptedFile()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+                {  @"c:\fasdrConfig.txt", new MockFileData("ThisShouldNotParse") }
+            });
+ 
+            var db = new Database(fileSystem);
+            db.Load();
+        }
+
+
+    }
+}

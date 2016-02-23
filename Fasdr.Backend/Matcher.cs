1901c51716aada98472623798b51aec2edabc6c3
Michael.Kelley <michael.a.kelley@gmail.com>
2016-02-22 23:28:48 -0800

    initial version of the matcher

--- Fasdr.Backend/Matcher.cs
+++ Fasdr.Backend/Matcher.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FuzzyString;
 
 namespace Fasdr.Backend
 {
     public static class Matcher
     {
-        public static IEnumerable<string> Matches(IDatabase db,string input)
+        public static IEnumerable<string> Matches(IDatabase db,string input,string provider)
         {
-            return null;
+            var results = new Dictionary<string,double>();
+            var opts = new List<FuzzyStringComparisonOptions>{ FuzzyStringComparisonOptions.UseLevenshteinDistance};
+            foreach (var e in db.Entries)
+            {
+                if (provider == e.Value.Provider &&
+                    ComparisonMetrics.ApproximatelyEquals(input, e.Key, opts, FuzzyStringComparisonTolerance.Weak))
+                    results.Add(e.Key,e.Value.Weight);
+            }
+
+            List<KeyValuePair<string, double>> myList = results.ToList();
+
+            myList.Sort((firstPair, nextPair) =>
+                {
+                    return nextPair.Value.CompareTo(firstPair.Value);
+                }
+            );
+
+            // sort and return:
+            return myList.ConvertAll(new Converter<KeyValuePair<string,double>, string>( p => p.Key));
         }
     }
 }
+    
--- Fasdr.UnitTests/MatcherTest.cs
+++ Fasdr.UnitTests/MatcherTest.cs
@@ -3,16 +3,38 @@ using NUnit.Framework;
 using Fasdr.Backend;
 using System.IO.Abstractions.TestingHelpers;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Fasdr.UnitTests
 {
     [TestFixture]
     public class MatcherTest
     {
+        IDatabase Db { get; set;  }
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            var mfs = new MockFileSystem(new Dictionary<string, MockFileData> {
+                {  Database.ConfigPath,
+                    new MockFileData(
+                        string.Join("" + Database.Separator,@"c:\dir1\testStr", "101.0", "FileSystem", "true") +
+                        Environment.NewLine +
+                        string.Join("" + Database.Separator,@"c:\testStr", "10.0", "FileSystem", "false") +
+                        Environment.NewLine)
+                }
+            });
+            var db = new Database(mfs);
+            db.Load();
+            Db = db;
+        }
+
         [Test]
         public void TestMatch()
         {
-            //CollectionAssert.IsNotEmpty(Matcher.Matches(new Database(new MockFileSystem()), "testStr"));
+            var matches = Matcher.Matches(Db, "testStr", "FileSystem");
+            Assert.AreEqual(2, matches.Count());
+            CollectionAssert.AreEqual(new List<string>{@"c:\dir1\testStr", @"c:\testStr"},matches);
         }
     }
 }
